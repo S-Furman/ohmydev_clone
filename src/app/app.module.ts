4585e046ede01695e1ed3c27a4a3244d1df6fc67
S-Furman <szymon.fur@o2.pl>
2022-04-09 19:48:43 +0200

    feat: add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./core/header/header.component";
+import {ThemeSwitchComponent} from "./core/header/theme-switch/theme-switch.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ThemeSwitchComponent
   ],
   imports: [
     BrowserModule,
